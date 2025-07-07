66c4f5bc8a14e6a803b6df1967624ddcfedf9bc0
andyhoanghieu <andyhoanghieu@gmail.com>
2025-07-08 03:10:09 +0700

    Create genre.js

---
+++ drxsw/src/genre.js
@@ -0,0 +1,12 @@
+function execute() {
+  return Response.success([
+    { title: "Huyền huyễn", input: "https://www.drxsw.com/xuanhuan/", script: "genrecontent.js" },
+    { title: "Võ Hiệp", input: "https://www.drxsw.com/wuxia/", script: "genrecontent.js" },
+    { title: "Đô Thị", input: "https://www.drxsw.com/dushi/", script: "genrecontent.js" },
+    { title: "Lịch Sử", input: "https://www.drxsw.com/lishi/", script: "genrecontent.js" },
+    { title: "Du Hí", input: "https://www.drxsw.com/youxi/", script: "genrecontent.js" },
+    { title: "Khoa Huyễn", input: "https://www.drxsw.com/kehuan/", script: "genrecontent.js" },
+    { title: "Khủng Bố", input: "https://www.drxsw.com/kongbu/", script: "genrecontent.js" },
+    { title: "Khác", input: "https://www.drxsw.com/qita/", script: "genrecontent.js" }
+  ]);
+}
