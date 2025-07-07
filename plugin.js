c92b31df21619b96c1ccc7f4377869aedea97558
andyhoanghieu <andyhoanghieu@gmail.com>
2025-07-08 03:25:48 +0700

    Update plugin.js tổng

--- plugin.js
+++ plugin.js
@@ -1,1 +1,15 @@
+{
+  "metadata": "vbook",
+  "description": "Tổng hợp các extension đọc truyện của Andy Hoàng Hiếu",
+  "plugins": [
+    {
+      "name": "Đông Nhật",
+      "url": "https://raw.githubusercontent.com/andyhoanghieu/vbook-extension/main/drxsw/plugin.json",
+      "lang": "zh",
+      "site": "https://www.drxsw.com",
+      "nsfw": false
+    },
+    
+  ]
+}
 
