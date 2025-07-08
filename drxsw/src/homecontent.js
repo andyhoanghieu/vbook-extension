95011e5c3c405dc5a30f68bc6f8bc27a9f4d25d5
andyhoanghieu <andyhoanghieu@gmail.com>
2025-07-08 19:34:18 +0700

    Create homecontent.js

---
+++ drxsw/src/homecontent.js
@@ -0,0 +1,32 @@
+// homecontent.js
+function execute(url, page) {
+  if (!page) page = "";
+
+  let response = fetch(url + page);
+  if (!response.ok) return null;
+
+  let doc = response.html();
+  const data = [];
+
+  doc.select(".section2 .bookbox").forEach(e => {
+    let name = e.select(".bookname").text();
+    let link = e.select("a").attr("href");
+    let cover = e.select(".bookimg img").attr("data-src");
+    let description = e.select(".intro_line").text();
+
+    data.push({
+      name: name,
+      link: link,
+      cover: cover,
+      description: description
+    });
+  });
+
+  // tìm nút phân trang tiếp theo
+  let next = doc.select(".pagelink .next").attr("href");
+  if (next && next !== "javascript:;") {
+    return Response.success(data, next);
+  }
+
+  return Response.success(data);
+}
