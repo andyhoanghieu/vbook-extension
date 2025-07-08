e9f5a97b4a3324efa7698a80a275d1166d401814
andyhoanghieu <andyhoanghieu@gmail.com>
2025-07-08 19:33:50 +0700

    Create home.js

---
+++ drxsw/src/home.js
@@ -0,0 +1,10 @@
+// home.js
+function execute() {
+  return Response.success([
+    {
+      title: "Truyện đề cử",
+      input: "https://www.drxsw.com/",
+      script: "homecontent.js"
+    }
+  ]);
+}
