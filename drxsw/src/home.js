fdeda3f6fe11dd52c7657926bc182e326c94e05e
andyhoanghieu <andyhoanghieu@gmail.com>
2025-07-09 08:26:00 +0700

    Update home.js

--- drxsw/src/home.js
+++ drxsw/src/home.js
@@ -1,9 +1,43 @@
-// home.js
 function execute() {
   return Response.success([
     {
-      title: "Truyện đề cử",
-      input: "https://www.drxsw.com/",
+      title: "玄幻小说",
+      input: "https://www.drxsw.com/xuanhuan/",
+      script: "homecontent.js"
+    },
+    {
+      title: "武侠小说",
+      input: "https://www.drxsw.com/wuxia/",
+      script: "homecontent.js"
+    },
+    {
+      title: "都市小说",
+      input: "https://www.drxsw.com/dushi/",
+      script: "homecontent.js"
+    },
+    {
+      title: "历史小说",
+      input: "https://www.drxsw.com/lishi/",
+      script: "homecontent.js"
+    },
+    {
+      title: "游戏小说",
+      input: "https://www.drxsw.com/youxi/",
+      script: "homecontent.js"
+    },
+    {
+      title: "科幻小说",
+      input: "https://www.drxsw.com/kehuan/",
+      script: "homecontent.js"
+    },
+    {
+      title: "恐怖小说",
+      input: "https://www.drxsw.com/kongbu/",
+      script: "homecontent.js"
+    },
+    {
+      title: "其他小说",
+      input: "https://www.drxsw.com/qita/",
       script: "homecontent.js"
     }
   ]);
