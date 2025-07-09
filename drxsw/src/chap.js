29e1575f03f136c38da5a27d3b4379d56cdb0198
andyhoanghieu <andyhoanghieu@gmail.com>
2025-07-09 08:10:40 +0700

    Update chap.js

--- drxsw/src/chap.js
+++ drxsw/src/chap.js
@@ -1,5 +1,12 @@
 function execute(url) {
     var doc = Http.get(url).html();
-    var content = doc.select("#TextContent").html();
+    var contentEl = doc.select("#TextContent");
+
+    // Xoá các đoạn quảng cáo hoặc script nếu có
+    contentEl.select("script").remove();
+    contentEl.select("ins").remove();
+    contentEl.select("iframe").remove();
+
+    var content = contentEl.html();
     return Response.success(content);
 }
