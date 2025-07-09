f13d22657557f25d7ff5b5636f29c6d4a273bcb8
andyhoanghieu <andyhoanghieu@gmail.com>
2025-07-09 08:06:10 +0700

    Update search.js

--- drxsw/src/search.js
+++ drxsw/src/search.js
@@ -1,28 +1,25 @@
 function execute(key, page) {
-  var url = "https://www.drxsw.com/s.php?searchkey=" + encodeURIComponent(key);
-  var response = fetch(url);
-  if (!response.ok) return Response.error("Lỗi tìm kiếm");
+    let doc = Http.post("https://www.drxsw.com/s.php", {
+        data: {
+            searchkey: key,
+            searchtype: "articlename"
+        }
+    }).html();
 
-  var doc = response.html();
-  var list = doc.select(".result-list li");
+    let list = [];
+    doc.select("dl#nr").forEach(dl => {
+        let name = dl.selectFirst("dd h3 a")?.text();
+        let link = dl.selectFirst("dd h3 a")?.attr("href");
+        let cover = dl.selectFirst("dt img")?.attr("data-src");
+        let description = dl.selectFirst("dd.book_des")?.text();
 
-  var data = [];
-
-  for (var i = 0; i < list.size(); i++) {
-    var el = list.get(i);
-    var name = el.select("h3 a").text();
-    var link = el.select("h3 a").attr("href");
-    var cover = el.select("img").attr("src");
-    var description = el.select(".result-game-item-desc").text();
-
-    data.push({
-      name: name,
-      link: link,
-      cover: cover,
-      description: description,
-      host: "https://www.drxsw.com"
+        list.push({
+            name,
+            link,
+            cover,
+            description
+        });
     });
-  }
 
-  return Response.success(data);
+    return Response.success(list);
 }
