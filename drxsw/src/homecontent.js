01e2caa581b660cc5821b8fce95f2247657f691c
andyhoanghieu <andyhoanghieu@gmail.com>
2025-07-09 10:42:22 +0700

    Update homecontent.js

--- drxsw/src/homecontent.js
+++ drxsw/src/homecontent.js
@@ -1,19 +1,18 @@
-// homecontent.js
 function execute(url, page) {
   var doc = Http.get(url).html();
-  var books = doc.select("div.channel dl");
+  var books = doc.select("dl#nr"); // mỗi truyện nằm trong <dl id="nr">
   var data = [];
 
   for (var i = 0; i < books.size(); i++) {
     var e = books.get(i);
-    var aTag = e.select("dd > a").first();
-    var imgTag = e.select("dt img").first();
-    var descTag = e.select("dd.name").first();
+    var aTag = e.select("dd h3 a").first();
+    var imgTag = e.select("img.lazy").first();
+    var descTag = e.select("dd.book_des").first();
 
     if (aTag != null) {
       var name = aTag.text();
       var link = aTag.attr("href");
-      var cover = imgTag != null ? imgTag.attr("src") : null;
+      var cover = imgTag != null ? imgTag.attr("data-src") : null;
       var description = descTag != null ? descTag.text() : "";
 
       data.push({
